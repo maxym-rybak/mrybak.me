135cfb16aa2fff35dea839650cf1ed0c594366e6
Maxym Rybak <hurubashi@gmail.com>
2020-06-03 20:20:00 +0300

    install typed.js

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,47 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Typed, { TypedOptions } from 'typed.js'
+import * as React from 'react'
 
-const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const AboutPage = () => {
+	const paragraphRef = React.useRef<HTMLParagraphElement>(null)
+	const paragraph2Ref = React.useRef<HTMLParagraphElement>(null)
+
+	React.useEffect(() => {
+		const options: TypedOptions = {
+			strings: ['This is the about page'],
+			typeSpeed: 50,
+			startDelay: 500,
+			showCursor: false,
+		}
+		if (paragraphRef.current) {
+			new Typed(paragraphRef.current?.tagName, options)
+		}
+		const options2: TypedOptions = {
+			strings: ['Second paragraph'],
+			typeSpeed: 50,
+			startDelay: 2000,
+			showCursor: false,
+		}
+		if (paragraph2Ref.current) {
+			new Typed(paragraph2Ref.current?.tagName, options2)
+			console.log(`tagname: ${paragraphRef.current?.tagName}`)
+		}
+	}, [])
+
+	return (
+		<Layout title='About | Next.js + TypeScript Example'>
+			<h1>About</h1>
+			<span ref={paragraphRef} id='typed-text'></span>
+			<p ref={paragraph2Ref} id='typed-text2'></p>
+
+			<p>
+				<Link href='/'>
+					<a>Go home</a>
+				</Link>
+			</p>
+		</Layout>
+	)
+}
 
 export default AboutPage
